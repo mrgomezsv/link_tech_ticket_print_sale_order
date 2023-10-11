a84de401c7a421f4a3a67f6b3c513ca2b9a896e1
Mario Roberto <mrg@treming.com>
2023-10-11 11:10:46 -0600

    Se agrego opcion Ticket de Venta en sale order

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "link_tech_ticket_print_sale_order",
+
+    'summary': """
+        Agrega opcion de imprimir ticket en sale order""",
+
+    'description': """
+        Agrega opcion de imprimir ticket en sale order
+    """,
+
+    'author': "Link Tech",
+    'website': "https://www.linktechsv.com/",
+
+    'category': 'Sale',
+    'version': '0.1',
+
+    'depends': ['base', 'sale'],
+
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/templates.xml',
+        'reports/report.xml'
+    ],
+}
